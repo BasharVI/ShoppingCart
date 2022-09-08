329606e9a25125fff3abd5a3fd35418cd6a012c7
Bashar V I <basharvismail@gmail.com>
2022-09-08 13:21:11 +0530

    addproduct form without image file consoled result

---
+++ routes/admin.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name:"realme c21",
+      category:"Mobile",
+      Description:'3 GB RAM | 32 GB ROM | Expandable Upto 256 GB',
+      image:"https://rukminim1.flixcart.com/image/416/416/ksnjp8w0/mobile/k/o/m/c21y-rmx3261-realme-original-imag65kcytrk8dtr.jpeg?q=70"
+
+    },
+    {
+      name:"POCO C30",
+      category:"Mobile",
+      Description:'13MP + 2MP + 2MP | 5MP Front Camera',
+      image:"https://rukminim1.flixcart.com/image/416/416/ku4ezrk0/mobile/p/e/4/c31-mzb0a0jin-poco-original-imag7bzqxgdhgf2n.jpeg?q=70"
+
+    },
+    {
+      name:"Samsung Galaxy F13",
+      category:"Mobile",
+      Description:'6000 mAh Lithium Ion Battery',
+      image:"https://rukminim1.flixcart.com/image/416/416/l4n2oi80/mobile/m/w/x/-original-imagfhu6bdzhnmkz.jpeg?q=70"
+    },
+    {
+      name:"RedMI 10A",
+      category:"Mobile",
+      Description:'16.59 cm (6.53 inch) Display',
+      image:"https://rukminim1.flixcart.com/image/416/416/l2f20sw0/mobile/h/c/6/10a-b09xb7hv7q-redmi-original-imagdrgp8pkbzccw.jpeg?q=70"
+    }
+  ]
+  res.render('admin/view-products',{admin:true,products})
+});
+
+router.get('/add-products',(req,res,next)=>{
+  res.render('admin/add-products',{admin:true})
+})
+router.post('/add-products',(req,res,next)=>{
+  res.send('got it')
+  console.log(req.body);
+})
+
+module.exports = router;
