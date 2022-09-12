7c4f5f4e243026feadf0c8adbdce0229d97e4f00
Bashar V I <basharvismail@gmail.com>
2022-09-12 14:58:49 +0530

    cartcount added

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@ var router = express.Router();
 const bcrypt = require('bcrypt');
 const { response } = require('express');
 const { ObjectId } = require('mongodb');
+const session = require('express-session');
 var MongoClient = require('mongodb').MongoClient
-const verifyLogin=(req,res,next)=>{
-  if (req.session.loggedIn){
+const verifyLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
     next()
-  }else{
+  } else {
     res.redirect('/login')
   }
 }
@@ -16,38 +17,66 @@ const verifyLogin=(req,res,next)=>{
 
 router.get('/', function (req, res, next) {
 
-  let user=req.session.user
-  
-  MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+  let user = req.session.user
+
+  MongoClient.connect('mongodb://localhost:27017', async function (err, client) {
     if (err)
       console.log('error');
     else
       function getallproducts() {
         return new Promise(async (resolve, reject) => {
+          let user = req.session.user
           let products = await client.db('shopping').collection('Products').find().toArray()
           resolve(products)
-          res.render('user/view-products', { admin: false, products,user })
+
         })
 
       }
-    getallproducts()
+    function getcartCount() {
+      return new Promise(async (resolve, reject) => {
+        let count = 0
+        if (req.session.loggedIn) {
+
+          let user = req.session.user._id
+
+          let cart = await client.db('shopping').collection('cart').findOne({ user: ObjectId(user) })
+
+          if (cart) {
+            count = cart.products.length
+          }
+        } else {
+          count = 0
+        }
+        resolve(count)
+
+
+
+      })
+    }
+
+    let cartcount = await getcartCount()
+
+    getallproducts().then((products) => {
+      res.render('user/view-products', { admin: false, products, user, cartcount })
+    })
 
   })
 
 });
 
-router.get('/login',(req, res)=> {
-  
-  if (req.session.loggedIn){
-    
+
+router.get('/login', (req, res) => {
+
+  if (req.session.loggedIn) {
+
     res.redirect('/')
 
   }
-  else{
-    res.render('user/login',{loginErr:req.session.loginErr})
-    req.session.loginErr=false
+  else {
+    res.render('user/login', { loginErr: req.session.loginErr })
+    req.session.loginErr = false
   }
- 
+
 })
 router.get('/signup', function (req, res) {
   res.render('user/signup')
@@ -71,9 +100,9 @@ router.post('/signup', function (req, res) {
         })
 
       }
-    dosignup().then((_id)=>{
-      req.session.loggedIn=true
-      req.session.user=_id
+    dosignup().then((_id) => {
+      req.session.loggedIn = true
+      req.session.user = _id
       res.redirect('/')
     })
 
@@ -101,7 +130,7 @@ router.post('/login', (req, res) => {
 
                 response.user = user
                 response.status = true
-                resolve({ status:true,user})
+                resolve({ status: true, user })
 
               } else {
                 console.log('erorrrrr');
@@ -120,15 +149,15 @@ router.post('/login', (req, res) => {
 
       }
     dologin().then((response) => {
-      
+
       if (response.status) {
-       
-        req.session.loggedIn=true
-        req.session.user=response.user
-        
+
+        req.session.loggedIn = true
+        req.session.user = response.user
+
         res.redirect('/')
       } else {
-        req.session.loginErr="Invalid UserName or Password !"
+        req.session.loginErr = "Invalid UserName or Password !"
         res.redirect('/login')
       }
     })
@@ -139,101 +168,107 @@ router.post('/login', (req, res) => {
 
 })
 
-router.get('/logout',(req,res)=>{
- req.session.destroy()
- res.redirect('/')
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
 })
 
-router.get('/cart',verifyLogin,(req,res)=>{
+router.get('/cart', verifyLogin, (req, res) => {
 
-  let userid=req.session.user._id
+  let userid = req.session.user._id
 
   MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     if (err)
       console.log('error');
     else
-    function getcartProducts(){
-      return new Promise(async(resolve ,reject)=>{
-        let cartItems=await client.db('shopping').collection('cart').aggregate([
-          {
-            $match:{user:ObjectId(userid)}
-          },
-          {
-            $lookup:{
-              from:'Products',
-              let:{proList:'$products'},
-              pipeline:[{
-                $match:{
-                  $expr:{
-                    $in:['$_id','$$proList']
-                  }
-                }
-              }],
-              as:'cartItems'
-            }
-          }
-
-        ]).toArray()
-          resolve(cartItems[0].cartItems)
-        
-        
-        
-      })
-    }
-    getcartProducts().then((response)=>{
-      let products=response
-      res.render('user/cart',{products,user:req.session.user})
-    })
-    
-  })
-
-  
-})
-
-router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
-
-  let proid=req.params.id
-  
-  let userid=req.session.user
-
-
-  MongoClient.connect('mongodb://localhost:27017', function (err, client) {
-    if (err)
-      console.log('error');
-    else
-
-  function addtoCart(){
-    return new Promise (async(resolve,reject)=>{
-      let usercart=await client.db('shopping').collection('cart').findOne({user:ObjectId(userid._id)})
-      if (usercart){
-        client.db('shopping').collection('cart')
-        .updateOne({user:ObjectId(userid._id)},
+      function getcartProducts() {
+        return new Promise(async (resolve, reject) => {
+          let cartItems = await client.db('shopping').collection('cart').aggregate([
             {
-              $push:{products:ObjectId(proid)}
-
+              $match: { user: ObjectId(userid) }
+            },
+            {
+              $lookup: {
+                from: 'Products',
+                let: { proList: '$products' },
+                pipeline: [{
+                  $match: {
+                    $expr: {
+                      $in: ['$_id', '$$proList']
+                    }
+                  }
+                }],
+                as: 'cartItems'
+              }
             }
-        ).then((response)=>{
-          resolve()
+
+          ]).toArray()
+          resolve(cartItems[0])
+
+
         })
-
-      }else{
-        let cartobj={
-          user:ObjectId(userid._id),
-          products:[ObjectId(proid)]
-        }
-
-        client.db('shopping').collection('cart').insertOne(cartobj).then((response)=>{
-          resolve()
-        })
-
       }
+    getcartProducts().then((response) => {
+      let product = response
+
+      if (product == null) {
+        res.render('user/cart')
+      } else {
+        let products = product.cartItems
+        res.render('user/cart', { products, user: req.session.user })
+      }
+
     })
-  }
-  addtoCart().then(()=>{
-    res.redirect('/')
+
   })
 
+
 })
+
+router.get('/add-to-cart/:id', verifyLogin, (req, res) => {
+
+  let proid = req.params.id
+
+  let userid = req.session.user
+
+
+  MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err)
+      console.log('error');
+    else
+
+      function addtoCart() {
+        return new Promise(async (resolve, reject) => {
+          let usercart = await client.db('shopping').collection('cart').findOne({ user: ObjectId(userid._id) })
+          if (usercart) {
+            client.db('shopping').collection('cart')
+              .updateOne({ user: ObjectId(userid._id) },
+                {
+                  $push: { products: ObjectId(proid) }
+
+                }
+              ).then((response) => {
+                resolve()
+              })
+
+          } else {
+            let cartobj = {
+              user: ObjectId(userid._id),
+              products: [ObjectId(proid)]
+            }
+
+            client.db('shopping').collection('cart').insertOne(cartobj).then((response) => {
+              resolve()
+            })
+
+          }
+        })
+      }
+    addtoCart().then(() => {
+      res.redirect('/')
+    })
+
+  })
 
 })
 
